6c50d5ddb462815839ad7c2f2c0ecc5283bc6851
Dominykas Kubiliūnas <97741934+StevieSunburn@users.noreply.github.com>
2022-04-19 08:59:42 +0200

    Update Drumpad.js

--- Drumpad.js
+++ Drumpad.js
@@ -2,7 +2,7 @@ const audio = new Audio();
 const Sounds = ["/Kick.wav", "/Piano.wav", "/Snare.wav", "/Clap.wav", "/Hit.wav", "/Snap.wav", "/OH.wav", "/Bell.wav", "/Trap.wav"]
 
 
-
+//function to play the sound
 function music(number){
     audio.src = Sounds[number]
     audio.play()
